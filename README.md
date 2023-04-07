fb62a31eba81925d76225de08f1d2d6366bad888
siva-TT <sivakumar@touchtier.com>
2023-04-07 15:21:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# signupappleid
+
+<img src="https://github.com/loydkim/sign_up_apple_id/blob/master/sign_up_apple_id.gif" width="300" height="560">
+
+Before Start it, you should set up a Firebase environment in your flutter project.
+
+** Please click the Image to know how it works **
+
+[![Youtube](https://img.youtube.com/vi/VzRWh5QB3U8/0.jpg)](https://youtu.be/VzRWh5QB3U8)
+
+* Develop environment.
+
+- Flutter SDK Version: 1.12.13+hotfix.9
+- Flutter: 45.1.1
+- Dart: 192.7761
+- Xcode Version: 11.4.1
+- Android Studio: 3.6.2
+- OS Version: MacOS Catalina 10.15.4
+
+#Flutter, #SignUpPhone, #SignUpAppleID
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,31 @@
+package io.flutter.plugins;
+
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.firebaseauth.FirebaseAuthPlugin;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+import com.aboutyou.dart_packages.sign_in_with_apple.SignInWithApplePlugin;
+import io.flutter.plugins.urllauncher.UrlLauncherPlugin;
+
+/**
+ * Generated file. Do not edit.
+ */
+public final class GeneratedPluginRegistrant {
+  public static void registerWith(PluginRegistry registry) {
+    if (alreadyRegisteredWith(registry)) {
+      return;
+    }
+    FirebaseAuthPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebaseauth.FirebaseAuthPlugin"));
+    FirebaseCorePlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebase.core.FirebaseCorePlugin"));
+    SignInWithApplePlugin.registerWith(registry.registrarFor("com.aboutyou.dart_packages.sign_in_with_apple.SignInWithApplePlugin"));
+    UrlLauncherPlugin.registerWith(registry.registrarFor("io.flutter.plugins.urllauncher.UrlLauncherPlugin"));
+  }
+
+  private static boolean alreadyRegisteredWith(PluginRegistry registry) {
+    final String key = GeneratedPluginRegistrant.class.getCanonicalName();
+    if (registry.hasPlugin(key)) {
+      return true;
+    }
+    registry.registrarFor(key);
+    return false;
+  }
+}
